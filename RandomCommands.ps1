e0814a3c1e3f8c476a2558ec1e32bd64cc7fdc18
Nuno Costa <ncosta@equalexperts.com>
2018-03-07 17:05:43 +0000

    Get-Member

--- RandomCommands.ps1
+++ RandomCommands.ps1
@@ -5,5 +5,10 @@ Get-WmiObject -Class Win32_ComputerSystem -Property Name
 
 Get-Command | Select-Object Name, Noun -Last 5 | Format-Table -AutoSize
 Get-Command | Select-Object Noun | Sort-Object Noun -Unique | measure
-Get-Command | Select-Object Noun | Sort-Object Noun | ? like "*user*"
+Get-Command | Select-Object Noun | Sort-Object Noun | ? Noun -Like "*user*"
 Get-Command | Select-Object Noun | Sort-Object Noun | Where-Object -Property Noun -EQ "*user*"
+Get-Help Get-Command -ShowWindow
+Show-Command Get-Command
+
+Get-ADUser -Filter * -Properties * | Select-Object -Last 1 | Get-Member | measure
+
