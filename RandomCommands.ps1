6dc8b6fdc1530e953631e9bb1a86b580204ebb78
Nuno Costa <ncosta@equalexperts.com>
2018-07-19 11:28:04 +0100

    Bulk commit

--- RandomCommands.ps1
+++ RandomCommands.ps1
@@ -4,13 +4,30 @@ Write-Host $ComputerName
 Get-WmiObject -Class Win32_ComputerSystem -Property Name
 
 Get-Command | Select-Object Name, Noun -Last 5 | Format-Table -AutoSize
-Get-Command | Select-Object Noun | Sort-Object Noun -Unique | measure
-Get-Command | Select-Object Noun | Sort-Object Noun | ? Noun -Like "*user*"
+Get-Command | Select-Object Noun | Sort-Object Noun -Unique | Measure-Command
+Get-Command | Select-Object Noun | Sort-Object Noun | Where-Object -Property Noun -Like "*user*"
 Get-Command | Select-Object Noun | Sort-Object Noun | Where-Object -Property Noun -EQ "*user*"
 Get-Help Get-Command -ShowWindow
 Show-Command Get-Command
 
-Get-ADUser -Filter * -Properties * | Select-Object -Last 1 | Get-Member | measure
+Get-ADUser -Filter * -Properties * | Select-Object -Last 1 | Get-Member | Measure-Command
+
+powershell -executionpolicy remotesigned -command build.ps1
+
+
+
+Add-Type -AssemblyName System.Speech
+$talk = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$talk.Speak("Hi Pluralsight Fans")
+
+$talk | Get-Member
+
+$talk.GetInstalledVoices() | Format-Table -AutoSize
+
+$talk.GetIntalledVoice().VoiceInfo | Get-Member
+
+Get-Service s* | Where-Object {$_.status -eq "running"} | Sort-Object -Property DisplayName | Select-Object -Property DisplayName
 
 # Modules
 $env:PSModulePath
